cb68ad4aff2760acea3818b8c0778fce217edb7f
Machindra2002 <128483348+Machindra2002@users.noreply.github.com>
2023-04-04 16:11:26 +0530

    Add files via upload

--- Intership/src/java/intenship/Array.java
+++ Intership/src/java/intenship/Array.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package intenship;
 
-/**
- *
- * @author lenovo
- */
+import java.util.*;
+
 public class Array {
+   
+     public static String[] UniqueArray(String[] names1, String[] names2) {
+
+        List<String> list = new ArrayList<String>();
+        for (String name : names1) {
+            list.add(name);
+        }
+        for (String name : names2) {
+            if (!list.contains(name)) {
+                list.add(name);
+            }
+        }
+        String[] a = list.toArray(new String[list.size()]);
+        return a;
+
+    }
     
+    public static void main(String args[]){
+         String[] names1 = new String[] { "kunal", "mandar", "abhi","jayesh" };
+        String[] names2 = new String[] { "abhi", "machindra", "laxman", "jayesh", "rahul" };
+
+        System.out.println(String.join(", ", Array.UniqueArray(names1, names2)));
+    }
 }
